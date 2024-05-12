2d7ddd4392a0f74395c3f652b848d385f574c4d0
Marc <info@marcsauer.net>
2024-05-12 12:08:12 +0200

    Added variation for ARM and Intel

--- Casks/trackaudio.rb
+++ Casks/trackaudio.rb
@@ -1,12 +1,19 @@
 cask "trackaudio" do
-    version "1.0.2-beta.3"
-    sha256 "cd4ecc4e8e4326ce4083c6ae9ed969efaffd090f2273cbf6e9a285891c715790"
-  
-    url "https://github.com/pierr3/TrackAudio/releases/download/1.0.2-beta.3/TrackAudio-1.0.2-beta.3-x64.dmg"
+    arch arm: "apple-silicon", intel: "x86_64"
+    version "1.0.2-beta.4"
+
+    on_arm do
+      sha256 "5430b7b795c746d805c1aa7058eddc29fdf09bb39c2c09978b63339ef1bc9da8"
+    end
+
+    on_intel do
+      sha256 "689e4daf6e042781cf8cab226bb662ccafd8cba4c874ac772471dc233ec24fed"
+    end
+    
+    url "https://github.com/pierr3/TrackAudio/releases/download/#{version}/TrackAudio-#{version}-#{arch}.dmg"
     name "TrackAudio"
     desc "A next generation Audio-For-VATSIM ATC Client for macOS, Linux and Windows."
     homepage "https://github.com/pierr3/TrackAudio"
   
     app "TrackAudio.app"
-  end
-  
+end
