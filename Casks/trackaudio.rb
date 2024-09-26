12e5e0a8c2d8839ee779f348df972c89d9284ae2
Marc Sauer <info@marcsauer.net>
2024-09-26 23:00:35 +0200

    Version 1.3.0-beta.2

--- Casks/trackaudio.rb
+++ Casks/trackaudio.rb
@@ -1,19 +1,19 @@
 cask "trackaudio" do
     arch arm: "apple-silicon", intel: "x64"
-    version "1.3.0-beta.1"
+    version "1.3.0-beta.2"
 
     on_arm do
-      sha256 "22672b2129ac6395ec23c0c495d5ddbe7a99b751ab2a85d8dc730207278a3fc0"
+      sha256 "2d994fe45f5deb2c3fd6e58e5e38303dbf716398a827ae0f0da41e7fb15cf712"
     end
 
     on_intel do
-      sha256 "7a847523805c04b5691b3473630bc2b4b3c17d45cee3756729cd964cfbe02df8"
+      sha256 "81d3ad6112c32b74a977510577ed61c607e39123a001199685c6dd244fa47629"
     end
-    
+
     url "https://github.com/pierr3/TrackAudio/releases/download/#{version}/TrackAudio-#{version}-#{arch}.dmg"
     name "TrackAudio"
     desc "A next generation Audio-For-VATSIM ATC Client for macOS, Linux and Windows."
     homepage "https://github.com/pierr3/TrackAudio"
-  
+
     app "TrackAudio.app"
 end
