de74e88f0ef210c94c74e5cd3373fcbd97aba7a9
Marc Sauer <info@marcsauer.net>
2024-11-02 11:53:51 +0100

    Version 1.3.0-beta3

--- Casks/trackaudio.rb
+++ Casks/trackaudio.rb
@@ -1,13 +1,13 @@
 cask "trackaudio" do
     arch arm: "apple-silicon", intel: "x64"
-    version "1.3.0-beta.2"
+    version "1.3.0-beta.3"
 
     on_arm do
-      sha256 "2d994fe45f5deb2c3fd6e58e5e38303dbf716398a827ae0f0da41e7fb15cf712"
+      sha256 "9af80d53a9452933c6e81e778a6edcff41d3a5baccf2ab116f42eeec2ab0e57c"
     end
 
     on_intel do
-      sha256 "81d3ad6112c32b74a977510577ed61c607e39123a001199685c6dd244fa47629"
+      sha256 "43f9d595bd857e1133404b7a7e144b83f2f8de299f36462bf7ed63207e5117a3"
     end
 
     url "https://github.com/pierr3/TrackAudio/releases/download/#{version}/TrackAudio-#{version}-#{arch}.dmg"
